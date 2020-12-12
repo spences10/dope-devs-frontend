ad7f02fd2f8562da2044980c1aea7aa72e83c9a4
spences10 <spences10apps@gmail.com>
2020-12-12 22:17:26 +0000

    render basic information

--- nextjs/pages/dev/[slug].js
+++ nextjs/pages/dev/[slug].js
@@ -1,16 +1,25 @@
 import sanityClient from "../../sanity-client";
 
-export default function DevPage() {
-  return <p>Yo OY</p>;
+export default function DevPage({ name, description }) {
+  return (
+    <>
+      <h1>{name}</h1>
+      <h3>{description}</h3>
+    </>
+  );
 }
 
-export async function getStaticProps() {
-  const devs = await sanityClient.fetch(
-    `*[_type == 'dev']{ 
-      'slug': slug.current 
+export async function getStaticProps({ params }) {
+  const slug = params.slug;
+  const [devs] = await sanityClient.fetch(
+    `*[_type == 'dev' && slug.current == '${slug}']{ 
+      _id,
+      name,
+      description,
+      likes
     }`
   );
-  return { props: {} };
+  return { props: { ...devs } };
 }
 
 export async function getStaticPaths() {
