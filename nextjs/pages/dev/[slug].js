ba444e5851693d3b72f02d30888a56a047560482
spences10 <spences10apps@gmail.com>
2020-12-13 14:12:56 +0000

    Add likes functionality

--- nextjs/pages/dev/[slug].js
+++ nextjs/pages/dev/[slug].js
@@ -1,10 +1,23 @@
+import { useState } from "react";
 import sanityClient from "../../sanity-client";
 
-export default function DevPage({ name, description }) {
+export default function DevPage({ name, description, likes, _id }) {
+  const [likeState, setLikes] = useState(likes);
+
+  const addLike = async () => {
+    const { likes: newLikes } = await fetch("/api/handle-likes", {
+      method: "POST",
+      body: JSON.stringify({ _id }),
+    }).then((response) => response.json());
+
+    setLikes(newLikes);
+  };
+
   return (
     <>
       <h1>{name}</h1>
       <h3>{description}</h3>
+      <button onClick={addLike}>{likeState} likes</button>
     </>
   );
 }
