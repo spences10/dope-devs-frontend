3363b00f80d974d797ee1b803850e98f0cf7af4e
spences10 <spences10apps@gmail.com>
2021-04-24 06:39:17 +0100

    add 404

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import { Layout } from '../components/layout'
+
+export default function NotFoundPage() {
+  return (
+    <Layout title="Page not found.">
+      <h1>404</h1>
+      <h4>Sorry that route doesn't exist.</h4>
+      <Link href="/">Go back home</Link>
+    </Layout>
+  )
+}
