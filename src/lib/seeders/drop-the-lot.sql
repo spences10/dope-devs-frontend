6faa2d9c0b336b338d996a9ab0b233552e23e737
spences10 <spences10apps@gmail.com>
2024-02-18 17:54:28 +0000

    chore: :wrench: drop shit

---
+++ src/lib/seeders/drop-the-lot.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS "__drizzle_migrations";
+-- DROP TABLE IF EXISTS country;
+DROP TABLE IF EXISTS dev_technology;
+DROP TABLE IF EXISTS dope_dev;
+DROP TABLE IF EXISTS session;
+DROP TABLE IF EXISTS technology;
+DROP TABLE IF EXISTS user;
