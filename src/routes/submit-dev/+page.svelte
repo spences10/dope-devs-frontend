f26f74193434744aaaea8c824799dc76a47e1c2f
spences10 <spences10apps@gmail.com>
2024-02-18 17:31:08 +0000

    style: :lipstick: add styles

--- src/routes/submit-dev/+page.svelte
+++ src/routes/submit-dev/+page.svelte
@@ -16,6 +16,7 @@
 <form
 	method="POST"
 	action="?/submit_dope_dev"
+	class="form-control space-y-4"
 	use:enhance={() => {
 		return ({ update, result }) => {
 			console.log('=====================');
@@ -25,30 +26,43 @@
 		};
 	}}
 >
-	<input
-		type="text"
-		bind:value={form_data.name}
-		name="name"
-		placeholder="Name"
-		required
-	/>
+	<label>
+		<div class="label">
+			<span class="label-text">Pick the best fantasy franchise</span>
+		</div>
+		<input
+			type="text"
+			bind:value={form_data.name}
+			name="name"
+			placeholder="Name"
+			required
+			class="input input-primary w-full max-w-xs"
+		/>
+	</label>
 	<input
 		type="text"
 		bind:value={form_data.title}
 		name="title"
 		placeholder="Title"
 		required
+		class="input input-primary w-full max-w-xs"
 	/>
 	<input
 		type="text"
 		bind:value={form_data.avatar}
 		name="avatar"
 		placeholder="Avatar URL"
+		class="input input-primary w-full max-w-xs"
 	/>
-	<textarea bind:value={form_data.bio} name="bio" placeholder="Bio"
-	></textarea>
+	<textarea
+		bind:value={form_data.bio}
+		name="bio"
+		placeholder="Bio"
+		class="textarea textarea-primary w-full max-w-xs"
+	/>
 	<select
 		bind:value={form_data.selected_country}
+		name="country_id"
 		class="select select-primary w-full max-w-xs"
 	>
 		<option disabled value="">Select a country</option>
@@ -56,7 +70,11 @@
 			<option value={country.id}>{country.name}</option>
 		{/each}
 	</select>
-	<button type="submit">Submit</button>
-</form>
 
-<pre>{JSON.stringify(data.countries, null, 2)}</pre>
+	<button
+		type="submit"
+		class="max-x-xs btn btn-primary w-full max-w-xs"
+	>
+		Submit
+	</button>
+</form>
