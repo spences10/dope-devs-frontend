9eaa98e6afd89946daecb5fb227d7fd7609e25cc
spences10 <spences10apps@gmail.com>
2024-02-18 17:20:02 +0000

    refactor: :recycle: update dev card

--- src/lib/components/developer.svelte
+++ src/lib/components/developer.svelte
@@ -1,11 +1,15 @@
-<script>
-	export let name, bio, image;
+<script lang="ts">
+	let { name, bio, image } = $props<{
+		name: string;
+		bio: string;
+		image: string;
+	}>();
 </script>
 
 <div class="card bordered compact card-side">
 	<div class="card-body flex">
 		<h2 class="card-title">{name}</h2>
-		<div>{@html bio}</div>
+		<div>{bio}</div>
 	</div>
 	<figure>
 		<img class="h-full w-full" src={image} alt={name} />
