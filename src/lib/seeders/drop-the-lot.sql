1999505200e859acd3e069d0775121f07bb7d072
spences10 <spences10apps@gmail.com>
2024-02-19 21:21:35 +0000

    refactor: :recycle: update seeders

--- src/lib/seeders/drop-the-lot.sql
+++ src/lib/seeders/drop-the-lot.sql
@@ -1,5 +1,5 @@
 DROP TABLE IF EXISTS "__drizzle_migrations";
--- DROP TABLE IF EXISTS country;
+DROP TABLE IF EXISTS country;
 DROP TABLE IF EXISTS dev_technology;
 DROP TABLE IF EXISTS dope_dev;
 DROP TABLE IF EXISTS session;
