365b4dbe211183a08d5330cdd075f43f7a7ec2d8
spences10 <spences10apps@gmail.com>
2024-02-26 21:03:16 +0000

    feat: :sparkles: add fathom

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,28 @@
-<script>
+<script lang="ts">
+	import { browser } from '$app/environment';
+	import { page } from '$app/stores';
+	import {
+		PUBLIC_FATHOM_ID,
+		PUBLIC_FATHOM_URL,
+	} from '$env/static/public';
 	import { Nav } from '$lib/components';
+	import * as Fathom from 'fathom-client';
 	import '../app.pcss';
 
 	let { children } = $props();
+
+	$effect(() => {
+		if (browser) {
+			Fathom.load(PUBLIC_FATHOM_ID, {
+				url: PUBLIC_FATHOM_URL,
+			});
+		}
+	});
+
+	// Track pageview on route change
+	$effect(() => {
+		$page.url.pathname, browser && Fathom.trackPageview();
+	});
 </script>
 
 <Nav />
